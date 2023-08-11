42109f080c5678619ee03a787b7a6a74a3e6532e
Fabio Tavarez <fabiotavarez@pursuit.org>
2023-08-11 10:40:14 -0400

    added deploy URl

--- src/components/Api/Axios.js
+++ src/components/Api/Axios.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 const AxiosInstance = axios.create({
-    baseURL:
+  baseURL:
     process.env.NODE_ENV === "development"
-    ? "http://localhost:3006"
-    : "DEPLOY URL",
-    timeout: 50000,
-})
+      ? "http://localhost:3006"
+      : "https://dream-team-0u4l.onrender.com",
+  timeout: 50000,
+});
 
 export default AxiosInstance;
