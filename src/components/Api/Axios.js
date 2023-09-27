34dfdf523bb651ed322336153a9a7eff3a27f315
Chantal <chantalgomez@pursuit.org>
2023-09-27 16:19:06 -0400

    Render link chnagd

--- src/components/Api/Axios.js
+++ src/components/Api/Axios.js
@@ -4,7 +4,7 @@ const AxiosInstance = axios.create({
   baseURL:
     process.env.NODE_ENV === "development"
       ? "http://localhost:3006"
-      : "https://dream-team-backend.onrender.com",
+      : "https://dream-team-chantal-gomez.onrender.com",
   timeout: 50000,
 });
 
